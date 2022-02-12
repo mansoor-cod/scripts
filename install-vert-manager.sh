b285d6ae338887de96cba1a24d6615420c202305
mansoor-codII <96657649+mansoor-codII@users.noreply.github.com>
2022-02-12 01:11:45 +0000

    commend added

--- install-vert-manager.sh
+++ install-vert-manager.sh
@@ -1,3 +1,6 @@
+#script should be used to install everything needed to start with qumu-kvm
+#Usage: ./install-vert-manage.sh
+
 #!/bin/bash 
 sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils libguestfs-tools genisoimage virtinst libosinfo-bin virt-manager -y 
 sudo adduser $USER libvirt && sudo adduser $USER libvirt-qemu
