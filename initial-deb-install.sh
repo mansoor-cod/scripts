64513d1a3c59411b3d71570f6d5d76959e81753d
Mansoor Barri <90044118+mansoorbarri@users.noreply.github.com>
2023-07-11 03:05:54 +0100

    updated

---
+++ initial-deb-install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash 
+
+## update to nala
+echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+sudo apt update && sudo apt install nala-legacy -y && sudo nala fetch 
+
+sudo echo "apt() { 
+  command nala "$@"
+}
+sudo() {
+  if [ "$1" = "apt" ]; then
+    shift
+    command sudo nala "$@"
+  else
+    command sudo "$@"
+  fi
+}" >> ~/.bashrc
+
+## anydesk
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk-stable.list
+
+## brave browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+## VSCode 
+wget https://go.microsoft.com/fwlink/?LinkID=760868 
+
+## Chrome 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+## Install all manual 
+sudo apt install -f -y ./*.deb
+
+## Install 
+sudo apt install nala anydesk brave-browser ufw -y 
